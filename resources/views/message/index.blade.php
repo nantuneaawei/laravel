90590fdaff7885a4f69117e1971f23f5cbdfd5e1
DESKTOP-53OTMRH\dahsh <dahshin2012@gmail.com>
2023-05-12 12:53:30 +0800

    startviews

---
+++ resources/views/message/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/app.css') }}">
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title></title>
+    </head>
+
+    <body>
+
+    </body>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</html>
